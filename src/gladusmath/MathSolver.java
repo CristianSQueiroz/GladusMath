8ab1b5f83d8600263f80a870f3c4090c6ee7ecaf
CristianSQueiroz <CristianSQueiroz@gmail.com>
2017-04-04 21:36:54 -0300

    LoadGeometricBasic

---
+++ src/gladusmath/MathSolver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gladusmath;
+
+/**
+ *
+ * @author csqueiroz
+ */
+public class MathSolver {
+    private String function;
+    
+    private Double solveFunction(String instruction){
+        return 0.0;
+    }
+}
